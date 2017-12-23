06e1bbd0f35df7840e251c4daccb7a61b1f64bb4
Shunt <shunt22@mail.ru>
2017-12-23 20:03:36 +0300

    fixes

--- src/main/java/ru/shunt/wowstat/controllers/MainController.java
+++ src/main/java/ru/shunt/wowstat/controllers/MainController.java
@@ -1,17 +1,13 @@
 package ru.shunt.wowstat.controllers;
 
-import java.io.IOException;
-
-import org.apache.http.client.ClientProtocolException;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
 	@RequestMapping("/")
-	public String index(Model model) throws ClientProtocolException, IOException {
+	public String index() {
 		return "index";
 	}
 }
